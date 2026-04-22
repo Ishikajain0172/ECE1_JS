62e2742d9d49b3bb95d22a11a9e9c5ff849984de
Ishikajain0172 <creationsmywonderful@gmail.com>
2026-04-22 10:04:29 +0530

    exp 6a uploaded

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
 import './App.css'
 
 function App() {
@@ -9,7 +15,17 @@ function App() {
 
   return (
     <>
-     
+     <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* Catch-all route for 404 Not Found */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+     </Router>
     </>
   )
 }
